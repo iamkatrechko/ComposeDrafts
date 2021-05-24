06eaa4d72beb412f3b3252d1a0b559c4d88cf35b
iamkatrechko <iamkatrechko@yandex.ru>
2021-05-24 13:19:34 +0300

    Добавление анимации смены цвета текста

--- app/src/main/java/com/iamkatrechko/drafts/compose/MainActivity.kt
+++ app/src/main/java/com/iamkatrechko/drafts/compose/MainActivity.kt
@@ -3,11 +3,14 @@ package com.iamkatrechko.drafts.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,10 +23,13 @@ class MainActivity : AppCompatActivity() {
                 Column {
                     Text(text = "Hello", color = MaterialTheme.colors.onSurface)
 
+                    val colorToggleState = remember { mutableStateOf(true) }
+                    val colorState = animateColorAsState(if (colorToggleState.value) MaterialTheme.colors.onSurface else Color.Green, animationSpec = tween(2000))
                     val secondTextState = remember { mutableStateOf("World") }
-                    Text(text = secondTextState.value, color = MaterialTheme.colors.onSurface)
+                    Text(text = secondTextState.value, color = colorState.value)
                     Button({
                         secondTextState.value = "User"
+                        colorToggleState.value = !colorToggleState.value
                     }) {
                         Text("Click me")
                     }
